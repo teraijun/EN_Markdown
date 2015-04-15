743078960756e2e31632ee051109b2d35bf356a8
teraijun <teraijun48@gmail.com>
2015-04-14 22:12:11 -0700

    add polls

--- evernote_oauth_sample/settings.py
+++ evernote_oauth_sample/settings.py
@@ -10,10 +10,11 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_DIR, 'UserInfo.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -25,7 +26,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -129,11 +130,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'oauth'
+    'oauth',
+    'polls',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
--- evernote_oauth_sample/urls.py
+++ evernote_oauth_sample/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns(
-    'oauth.views',
-    url(r"^$", "index", name="evernote_index"),
-    url(r"^auth/$", "auth", name="evernote_auth"),
-    url(r"^login/$", "login", name="evernote_login"),
-    url(r"^info/$", "get_info", name="evernote_info"),
-    url(r"^reset/$", "reset", name="evernote_auth_reset"),
-    url(r"^note/$", "note", name="evernote_note"),
+    '',
+    url(r"^$", "oauth.views.index", name="evernote_index"),
+    url(r"^auth/$", "oauth.views.auth", name="evernote_auth"),
+    url(r"^login/$", "oauth.views.login", name="evernote_login"),
+    url(r"^info/$", "oauth.views.get_info", name="evernote_info"),
+    url(r"^reset/$", "oauth.views.reset", name="evernote_auth_reset"),
+    url(r"^note/$", "oauth.views.note", name="evernote_note"),
+    url(r'^admin/', include(admin.site.urls)),
 )
