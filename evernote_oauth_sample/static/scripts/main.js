fe918619836b645e523348d0f19ada3aae6d770c
Jun Terai <jterai-nikkei@Jun-Terai-Nikkeis-MBA.local>
2015-04-28 13:24:40 -0700

    modify fron-end

--- evernote_oauth_sample/static/scripts/main.js
+++ evernote_oauth_sample/static/scripts/main.js
@@ -1,8 +1,7 @@
 "use strict";
 
-var attached_files = [];
-
 $(function(){
+  var attached_files = [];
   marked.setOptions({
     renderer: new marked.Renderer(),
     gfm: true,
@@ -21,10 +20,28 @@ $(function(){
     return marked(str);
   }
 
+
+  function getCookie(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+          var cookies = document.cookie.split(';');
+          for (var i = 0; i < cookies.length; i++) {
+              var cookie = jQuery.trim(cookies[i]);
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+  }
+
+
   new Vue({
     el: '#content',
     data: {
-      input: '# hello',
+      input: '',
       response:{},
       clipped:{},
       selected: '',
@@ -44,7 +61,7 @@ $(function(){
             that.$data.response.redirect_url = response.redirect_url+'?callback='+encodeURIComponent(window.location.href);
           }
         });
-        $('.input_area_all').bind('drop', function(e){
+        $('#input_area').bind('drop', function(e){
             // デフォルトの挙動を停止
             e.preventDefault();
          
@@ -63,6 +80,8 @@ $(function(){
         clip: function($e){
           var that = this;
           that.$data.fd.append('title', $('#title').val() || 'Untitled');
+          var input_val = $('#input_area').val();
+          $('#body').html(marked2(input_val));
           var body = $('#body').html().replace(/ (id|class)[^>]+/g, '').replace(/(<hr>)/g, '$1</hr>').replace(/(<img[^>]+>)/g,'$1</img>');
 
           _.each(attached_files, function(a){
@@ -89,7 +108,11 @@ $(function(){
               console.log(response);
               if (response.status != 'error'){
                 that.$data.clipped = response;
-                $('#div-modal').modal();
+                $('#clip').attr('data-content', response.note.title);
+                $('#clip').popover('toggle');
+                setTimeout(function(){
+                  $('#clip').popover('hide');
+                },3000)
               }
             }
           });
@@ -117,37 +140,26 @@ $(function(){
                 var input = $('#input_area');
                 var input_val = input.val();
                 var pos = input.caretPos();
-                var id = file.name+'_'+(new Date().getTime());
+                var id = file.name+'_'+(new Date().getYear());
                 var insert = '\n\n !['+file.name+']('+id+' "'+file.name+'") \n\n';
                 input_val = input_val.substr(0,pos) + insert + input_val.substr(pos, input_val.length);
                 input.val(input_val);
                 var obj = {id:id, name: file.name, result: this.result};
                 attached_files.push(obj);
-                $('#body').html(marked2(input_val));
               }}(file, i);
               reader.readAsDataURL(file);
               that.$data.fd.append('files[]', file);
+              console.log('upload');
+            } else {
+              console.log('upload only img file');
             }
           }
-          console.log('アップロード');
+        },
+        showPreview: function(){
+          var input_val = $('#input_area').val();
+          $('#body').html(marked2(input_val));
+          $('#div-modal').modal();
         }
       }
   });
 });
-
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
